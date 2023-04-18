5f8c83155bc765dd0fc0d5ca2a575a0f389adab6
DmitriyKrasnikov <116814562+DmitriyKrasnikov@users.noreply.github.com>
2023-04-18 04:16:08 +0300

    б

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,10 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.util.HashMap;
-import java.util.List;
-
 @Component
-public interface UserStorage extends MainStorage<User>{
+public interface UserStorage extends MainStorage<User> {
 
 }
