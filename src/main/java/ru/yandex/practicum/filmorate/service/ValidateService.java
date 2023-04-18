cd3f81cf72e9425a62fe33ebb7a20384bb99cb32
DmitriyKrasnikov <116814562+DmitriyKrasnikov@users.noreply.github.com>
2023-04-18 04:09:11 +0300

    и

--- src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ValidateService.java
@@ -1,8 +1,8 @@
 package ru.yandex.practicum.filmorate.service;
 
 import lombok.extern.slf4j.Slf4j;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -14,18 +14,18 @@ import java.util.ArrayList;
 public class ValidateService {
 
     public void validateUser(User user) {
-        if(user.getName().isBlank()){
+        if (user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
     }
 
     public void validateFilm(Film film) throws ValidationException {
-        if(film.getReleaseDate() == null ||
-                film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))){
+        if (film.getReleaseDate() == null || film.getReleaseDate()
+                .isBefore(LocalDate.of(1895, 12, 28))) {
             log.debug("Фильм не прошёл валидацию: {}", film);
             throw new ValidationException("Ошибка валидации фильма");
         }
-        if(film.getGenres() == null){
+        if (film.getGenres() == null) {
             film.setGenres(new ArrayList<>());
         }
     }
