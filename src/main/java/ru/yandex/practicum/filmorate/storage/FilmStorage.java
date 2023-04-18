60438b64ad0a4e7d91f05edb60c18e548f1a5010
DmitriyKrasnikov <116814562+DmitriyKrasnikov@users.noreply.github.com>
2023-04-18 04:11:57 +0300

    т

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -3,9 +3,7 @@ package ru.yandex.practicum.filmorate.storage;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.util.HashMap;
-
 @Component
-public interface FilmStorage extends MainStorage<Film>{
+public interface FilmStorage extends MainStorage<Film> {
     int generateId();
 }
